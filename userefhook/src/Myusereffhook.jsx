699c277710e169e7b1945d5dc27a2c699482367c
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-10 19:25:56 +0300

    document the usereff project

--- userefhook/src/Myusereffhook.jsx
+++ userefhook/src/Myusereffhook.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { useRef } from 'react'
 
 function Myusereffhook() {
-
+// initialize a usereff hook
     const Usereffhook = useRef(null)
-
+// create a funtion to be handled on click
     function handlebeautifyelement(){
         Usereffhook.current.classList.toggle("myhook")
+        // Note:  L in classlist is Capital
     }
   return (
     <>
-    <div ref={Usereffhook}>Myusereffhook</div>
+    {/* pass a ref inside the container you want to change */}
+    <div ref={Usereffhook}>Myusereffhook</div> 
     <button onClick={handlebeautifyelement}>beautify</button>
     </>
   )
