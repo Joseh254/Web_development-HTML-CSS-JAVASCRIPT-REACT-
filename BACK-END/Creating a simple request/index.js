a37e560fbf81fb921414028dd8db2a04083ccb65
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-20 17:04:09 +0300

    add a call

--- BACK-END/Creating a simple request/index.js
+++ BACK-END/Creating a simple request/index.js
@@ -7,6 +7,23 @@ app.get("/users", (request,response)=>{
 
 })
 
+app.get("/movies", (request,response)=>{
+    response.send(['Action','comedy','animation'])
+})
+
+// separating controller from its parent
+const getmusic=(request,response)=>{
+    response.send(['Afrobeats','pop','hiphop'])
+}
+app.get("/music",getmusic)
+
+
+function getseries(request,response){
+    response.send(['The terminalist','End game','Squid game','Nikita','Person of interest'])
+}
+
+app.get("/series", getseries)
+
 app.listen(3000, () => {
   console.log("App running on port 3000...");
 });
